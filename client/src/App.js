2aafbe479db927cc09459d43b4e75e14e346ca47
jlayher <john.e.layher@gmail.com>
2021-04-13 19:23:08 -0400

    Fixed issues with connecting to DB

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,23 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('localhost:5000/api/courses')
+    fetch('http://localhost:5000/api/courses')
     .then(res => res.json())
-    .then(res => console.log(res))
     .then((resData) => {
       this.setState({
         data: resData
       });
-      console.log(data);
     })
     .catch(console.log)
   }
   render(){
     return (
       <div>
-        <span>{data}</span>
+        {this.state.data.map(course => 
+          <div>
+            {course.description}
+          </div>
+        )}
       </div>
     );
   }
