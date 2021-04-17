4a2eed89cedf16412ef20c8560d7a39354a12a11
jlayher <john.e.layher@gmail.com>
2021-04-16 23:15:07 -0400

    Import React in Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ It displays a menu bar for the application and includes
     buttons for signing up (as long as the user is not currently authenticated)
     the user's name and a button for signing out (when there is an authenticated user)
 */
-
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Header = () => {
