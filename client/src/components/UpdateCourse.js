cf51d9c5e21324906cb045cb7272893e8d99119c
jlayher <john.e.layher@gmail.com>
2021-04-21 22:56:45 -0400

    Fixed the UpdateCourse Component

--- client/src/components/UpdateCourse.js
+++ client/src/components/UpdateCourse.js
@@ -18,24 +18,22 @@ class UpdateCourse extends Component {
     constructor(props) {
         super(props);
         this.state ={
-            title: '',
-            description: '',
+            courseTitle: '',
+            courseDescription: '',
             estimatedTime: '',
             materialsNeeded: '',
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-//component did mount func
-    //axios request for the course's data
 
     async componentDidMount() {
         const { id } = this.props.match.params;
         await axios.get(`http://localhost:5000/api/courses/${id}`)
             .then(res => {
                 this.setState({
-                    title: res.data.title,
-                    description: res.data.description,
+                    courseTitle: res.data.title,
+                    courseDescription: res.data.description,
                     materialsNeeded: res.data.materialsNeeded,
                     estimatedTime: res.data.estimatedTime,
                 });
@@ -45,20 +43,10 @@ class UpdateCourse extends Component {
             });
     }
 
-
-//handleChange function
-    //bind to "this" in constructor
-    //take in event object as parameter
-    //update state
-
     handleChange(event) {
         this.setState({ [event.target.name] : event.target.value });
      }
 
-
-//SOMETHING IS WRONG WITH THE AXIOS PUT AND PUSH REQUESTS
-//this one is not executing the update to the database
-//nor is it properly redirecting to the course description after submission
     handleSubmit = (event) => {
         event.preventDefault();
         const {id} = this.props.match.params;
@@ -71,8 +59,8 @@ class UpdateCourse extends Component {
                 password: Cookies.get('password')
             },
             data: {
-                title: this.state.title,
-                description: this.state.description,
+                title: this.state.courseTitle,
+                description: this.state.courseDescription,
                 estimatedTime: this.state.estimatedTime,
                 materialsNeeded: this.state.materialsNeeded
             }
@@ -85,9 +73,10 @@ class UpdateCourse extends Component {
         })
     }
 
-
     render(){
         const { id } = this.props.match.params;
+        const placeholderTitle = this.state.courseTitle;
+        const placeholderDescription = this.state.courseDescription;
         return(
             <Consumer>
                 {context => (
@@ -98,20 +87,48 @@ class UpdateCourse extends Component {
                                 <div className="main--flex">
                                     <div>
                                         <label htmlFor="courseTitle">Course Title</label>
-                                        <input id="courseTitle" name="courseTitle" type="text" value={this.state.title} onChange={this.handleChange} />
+                                        <input 
+                                            id="courseTitle" 
+                                            name="courseTitle" 
+                                            type="text" 
+                                            placeholder={placeholderTitle} 
+                                            value={this.state.Coursetitle} 
+                                            onChange={this.handleChange} />
 
                                         <label htmlFor="courseAuthor">Course Author</label>
-                                        <input id="courseAuthor" name="courseAuthor" type="text" value={`${context.state.firstName} ${context.state.lastName}`} onChange={this.handleChange} />
+                                        <input 
+                                            id="courseAuthor" 
+                                            name="courseAuthor" 
+                                            type="text" 
+                                            value={`${context.state.firstName} ${context.state.lastName}`} 
+                                            onChange={this.handleChange} />
 
                                         <label htmlFor="courseDescription">Course Description</label>
-                                        <textarea id="courseDescription" name="courseDescription" value={this.state.description} onChange={this.handleChange}></textarea>
+                                        <textarea 
+                                            id="courseDescription" 
+                                            name="courseDescription" 
+                                            placeholder={this.state.courseDescription}
+                                            value={placeholderDescription} 
+                                            onChange={this.handleChange}>
+                                        </textarea>
                                     </div>
                                     <div>
                                         <label htmlFor="estimatedTime">Estimated Time</label>
-                                        <input id="estimatedTime" name="estimatedTime" type="text" placeholder={this.state.estimatedTime} value={this.state.estimatedTime} onChange={this.handleChange}/>
-
+                                        <input 
+                                            id="estimatedTime" 
+                                            name="estimatedTime" 
+                                            type="text" 
+                                            placeholder={this.state.estimatedTime} 
+                                            value={this.state.estimatedTime} 
+                                            onChange={this.handleChange}/>
+                                            
                                         <label htmlFor="materialsNeeded">Materials Needed</label>
-                                        <textarea id="materialsNeeded" name="materialsNeeded" placeholder={this.state.materialsNeeded} value={this.state.materialsNeeded} onChange={this.handleChange}></textarea>
+                                        <textarea 
+                                            id="materialsNeeded" 
+                                            name="materialsNeeded" 
+                                            placeholder={this.state.materialsNeeded} 
+                                            value={this.state.materialsNeeded} 
+                                            onChange={this.handleChange}></textarea>
                                     </div>
                                 </div>
                                 <NavLink className="button" to={`/courses/${id}`} onClick={this.handleSubmit}>Update Course</NavLink>
